d5555cf907ad45d3f83949b9d635c1a0436c158b
Jeydin Pham <89167700+Jeydin@users.noreply.github.com>
2021-10-22 16:12:55 +0000

    Finished Strings notes

--- slide_practice.py
+++ slide_practice.py
@@ -26,16 +26,16 @@ print("==== String Methods ====")
 # Length
 # len() function
 print(len(s))
-print(s[len(s) - 1]) # 12
+print(s[len(s) - 1])  # 12
 
 # .find - Finds the location of a specific character
 print(s.find("p"))
-print(s.find("x")) # Returns -1 since it can't find it
+print(s.find("x"))  # Returns -1 since it can't find it
 
 # .rfind - Finds the location of a specific character
 # But from the reverse direction (right to left)
-print(s.find("c")) # Left to right, should be 0
-print(s.rfind("c")) # Right to left, shoud be 11
+print(s.find("c"))  # Left to right, should be 0
+print(s.rfind("c"))  # Right to left, shoud be 11
 
 # Concatenation
 
@@ -53,3 +53,17 @@ if len(u) < len(v):
 	print(v + " wins!")
 else:
 	print(u + " wins!")
+
+# ASCII Notes
+print()
+print("=== ASCII Character Set ===")
+# ord()
+# Grabs the ASCII code associated with the character
+print(ord("A"))
+
+# chr()
+print(chr(66))
+print(chr(98))
+
+print("computers" < "computing")
+# Helps with alphabetical order
